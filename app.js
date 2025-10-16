43e7d9c323879e24d922a508ceedb33ffbfa1c55
Amado ponce <ponceamado69@gmail.com>
2025-10-16 17:10:50 -0300

    avance del proyecto

--- app.js
+++ app.js
@@ -1,262 +1,116 @@
-// variable es un de memoria en nuestro sitio web
-
-// declaracion 
-// let nombre = "valentine";  // string
-// let edad = 32; // number
-// let esdesarrollador = true; //boolean
-
-// const dni = 3300000 // number
 
 
-// console.log(nombre);
-// console.log(edad);
 
-// nombre = "pamela";
-
-// let num1 = 8;
-// let num2 = 5;
-// let num3 = "1";
-
-// console.log(num1 + num2);
-// console.log(num1 * num2);
-
-
-// let nombreIngresado = prompt("ingrese nombre");
-// console.log("Hola " + nombreIngresado);
-
-// alert("bienvenidos al curso de javscript");
-
-// let continuar = confirm("¿Estas seguro que queres continuar?");
-// console.log(continuar);
-
-
-// let nombre = "daniela";
-// console.log(nombre);
-
-// let esprogramador = "true";
-// console.log(esprogramador)
-
-// let num1 = 458;
-// let num2 = 269;
-// let num3 = "748";
-// let num4 = 852;
-
-// console.log(num3 / num4);
-
-// condicionesles
-
-
-// let peso = prompt("ingrese su peso");
-
-// if (edad !==20) {
-// console.log("sos mayor de edad");
-// }
-
-
-// if (edad > 20) {
-// console.log("sos mayor de edad");
-// }
-
-// AND siempre que todas las condiciones sean true el operador sera true  
-// if (!(edad >= 20 && edad <= 30)) {
-//     console.log("podes entrar");
-// }
-
-// OR siempre que una condicion sea true, el operador OR sera true
-
-// if (edad >= 20 || edad <= 30) {
-//     console.log("podes entrar");
-// }
-
-// let edad;
-// let conalguien;
-// if (edad < 20) {
-//     conalguien = confirm("Estas acompañado/a");
-// }
-
-// if (edad >= 20) {
-// console.log("sos mayor de edad, podes pasar");
-// } else if ((edad == 23 || edad == 24) && conalguien) {
-//     console.log("sos menor de edad, pero podes pasar acompañado/a");
-// } else {
-//     console.log("Sos menor de edad, no podes pasar");
-// }
-
-// Switch es muyu similar al if. nos permite evaluar el valor de una variable de acuerdo a distintos valores y depediendo el caso ejecutamos.
-// usamos switch solamente para evaluamos condiciones de igualaddad
-
-
-// switch (parseInt(peso)) {
-//     case 90:
-//         console.log("pesas arriba de 90, entra");
-//         break;
-
-//     default:
-//         console.log("Estas en el lugar equivocado");
-        
-//         break;
-// }
-
-// Funciones es bloque de codigos
-
-// function saludar(){
-//     console.log("hola mundo");
-// }
-
-// saludar();
-
-// function buscarpantalon(LasOreiro) {
-//     console.log("buscarpantalon: ${LasOreriro}...");
-
-// }
-
-// buscarpantalon("LasOreiro");
-
-// function saludar (repes){
-
-
-//     for (let i = 0; i < repes; i++) {
-// saludar();
-//     console.log("Hola mundo");
-//     }
-// }
-// saludar(5);
-
-// function saludarusuario(nombre){
-//     alert("hola" + nombre);
-// }
-// let nombreusuario = prompt("ingrese su nombre");
-// saludarusuario(nombreusuario);
-
-// function sumar(num1, num2){
-//     // resultado es una variable local
-//     let resultado = num1 + num2;
-// return resultado;
-// }
-// // resultado de una variable global
-// let resultadodesuma = sumar(5,20);
-// console.log(resultadodesuma);
-
-// function mostrarturnos(cantTurnos){
-//     let cantidadDeTurnos = 0;
-//     for (let i = 1; i <= cantTurnos; i++ ) {
-//         cantidadDeTurnos++;
-//     }
-//     console.log('se han otorgado ${cantidadDeTurnos} turnos');
-// }
-
-
-// mostrarturnos(22);
-
-//funciones anonimas
-
-// (function(){
-// console.log("soy una funcion anonima");
-// })();
-
-// funciones flechas es mejor mejor manejo para operaciones mas complejas
-
-// let sumar = (num1, num2) => {
-//     return num1 + num2;
-// }
-
-// let resultado = sumar(45, 85);
-
-// console.log(resultado);
-
-// let sumar = (num1, num2) => num1 + num2;
-// let restar = (num1, num2) => num1 - num2;
-// let multiplicar = (num1, num2) => num1 * num2;
-// let dividir = (num1, num2) => num1 / num2;
-
-// const calculadora = (num1, num2, operaciones) => {
-//     switch (operacion) {
-//         case "1":
-//             console.log(sumar(num1, num2));
-//             break;
-//     case "2":
-//         console.log(restar(num1, num2));
-//             break;
-//             case "3":
-//                 console.log(multiplicar(num1, num2));
-//             break;
-//             case "4":
-//                 if (num2 === 0){
-//                     console.error("error: no se puede dividir por cero");
-//                 } else {
-// console.log(dividir(num1, num2));
-//                 }
-//             break;
-//         default:
-//             console.log("operacion no valida");
-//             break;
-//     }
+// const usuario = {
+//     id: "1234",
+//     nombre: "Amado",
+//     correo: "Amado@example.com"
 // };
 
-// let numero1 = parseInt(prompt("ingrese el primer numero"));
-// let numero2 = parseInt(prompt("ingrese el segundo numero"));
-// let operacion = prompt("Que operacion quiere realizar? 1)sumar 2)restar 3)multiplicar 4)dividir");
+// localStorage.setItem('usuario', JSON.stringify(usuario));
 
-// calculadora(numero1, numero2, operacion)
+// const usuarioAlmacenado =
+// JSON.parse(localStorage.getItem('usuario'));
 
-//ejemplo en vivo de iva
+// console.log(usuarioAlmacenado);
 
-// const IVA = 1.21;
+// Ejemplo de uso de localStorage
 
-// let agregarIVA = (precio) => precio * IVA;
-
-// let calculofinal = agregarIVA(parseInt(prompt("ingrese el precio del producto")));
-
-
-// let arrayobjetos =[];
-
-// arrayobjetos.push({
-//     id:1 , nombre: "producto1"
-// })
-// arrayobjetos.push({
-//     id:2 , nombre: "producto2"
-// })
-// arrayobjetos.push({
-//     id:3 , nombre: "producto3"
-// })
-// console.log(arrayobjetos);
-
-// for(let objeto of arrayobjetos){
-//     console.log(objeto.nombre);
+// let usuario = {
+//     nombre: "Amado",
+//     edad: 30
 // }
 
-// let numero = [1, 2, 3, 4, 5, 6, ];
+// console.log(usuario);
 
-// console.log(numero);
+// let usuarioJSON = JSON.stringify(usuario);
+// console.log(usuarioJSON);
 
-// let palabras = ["auto", "manzana", "perra", "programacion"];
+// localStorage.setItem('usuario', usuarioJSON);
 
-// console.log(palabras);
+// let usuariorecuperado = localStorage.getItem('usuario');
+// console.log(usuariorecuperado);
 
-// console.log(palabras[2]); 
+// let usuarioObjeto = JSON.parse(usuariorecuperado);
 
-// let booleanos = [true, false, true, false];
+// console.log(usuarioObjeto);
 
-// console.log(booleanos);
+// let productos = {
+//     nombre: "pantalon",
+//     precio: 7000,
+//     diseño: "jean"
+// }
+// console.log(productos);
 
-// let matriz = [
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-// ];
+// let productosJSON = JSON.stringify(productos);
+// console.log(productosJSON);
 
-// console.log(matriz);
+// localStorage.setItem('productos', productosJSON);
 
-// console.log(matriz[0][2]);
+// let productosRecuperados = localStorage.getItem('productos');
+//     console.log(productosRecuperados);
 
-let numeros = [40,5,200,9];
+// let productosObjeto = JSON.parse(productosRecuperados);
 
-numeros.sort((a,b) => a -b );
-console.log(numeros);
+// console.log(productosObjeto);
 
-let palabras = ["auto", "campera", "vasija", "computadora"];
-palabras.sort();
-console.log(palabras) ;
+// let carrito = ["auto", "moto", "camioneta"];
+// console.log(carrito);
 
-numeros.reverse();
-console.log(numeros);
+// let carritoJSON = JSON.stringify(carrito);
+// console.log(carritoJSON);
+
+// localStorage.setItem('carrito', carritoJSON);
+
+// let carritoRecuperado = localStorage.getItem('carrito');
+// console.log(carritoRecuperado);
+
+// let carritoObjeto = JSON.parse(carritoRecuperado);
+// console.log(carritoObjeto);
+
+
+// let muebles = {
+//     nombre: "sillon",
+//     precio: 65000,
+//     color: "gris",
+//     material: "cuero"
+// }
+// console.log(muebles);
+
+// let mueblesJSON = JSON.stringify(muebles);
+
+// console.log(mueblesJSON);
+
+// localStorage.setItem('muebles', mueblesJSON(muebles));
+
+// let mueblesRecuperados = localStorage.getItem('muebles');
+
+// console.log(mueblesRecuperados);
+
+// let mueblesObjeto = JSON.parse(mueblesRecuperados);
+
+// console.log(mueblesObjeto);
+
+const carrito = {
+    items: [
+        {id: 1, nombre: "auto", precio: 20000, cantidad: 4},
+        {id: 2, nombre: "moto", precio: 10000, cantidad: 3}
+    ]
+        
+}
+
+console.log(carrito);
+
+localStorage.setItem('carrito', JSON.stringify(carrito));
+
+const carritoRecuperado = JSON.parse(localStorage.getItem('carrito'));
+
+console.log(`total de productos: ${carritoRecuperado.items.length}`);
+
+const total = carritoRecuperado.items.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
+
+console.log(`total a pagar: ${total}`);
+
+const = [
+    id
+]
--- ropa/ropa.js
+++ ropa/ropa.js
@@ -1,17 +1,69 @@
 // Variables para la ropa
 
+let Ropamas = document;
+
+let contenedor = Ropamas.getElementById("unico");
+
+console.log(contenedor);
+
 let camisa = "camisa de algodon";
 let pantalon = "pantalon camuflado";
 let zapatillas = "zapatillas deportivas";
 let buzo = "buzo overside";
-let remera = "remera estampada"
+let remera = "remera estampada";
+let sandalias = "sandalias de cuero";
+let gorra = "gorra deportiva";
+let short = "short de jean";
+let chaqueta = "chaqueta de cuero";
+let vestido = "vestido de seda";
 
-alert("Bienvenido a la tienda de ropa");
-
-let ropa = prompt("Que prenda queres comprar?");
-while(ropa != "camisa" && ropa != "pantalon" && ropa != "zapatillas" && ropa != "buzo" && ropa != "remera") {
-    ropa = prompt("Prenda no disponible. Que prenda queres comprar?");
+function mostrarPrenda(prenda) {
+    console.log("La prenda es: " + prenda);
+    alert("La prenda es: " + prenda);
 }
+
+
+let carrito = [];
+
+const productos = [
+    {id: 1, prenda: "camisa de algodon", precio: 3500},
+    {id: 2, prenda: "pantalon camuflado", precio: 7000},
+    {id: 3, prenda: "zapatillas deportivas", precio: 24000},
+    {id: 4, prenda: "buzo overside", precio: 6000},
+    {id: 5, prenda: "remera estampada", precio: 2500},
+    {id: 6, prenda: "sandalias de cuero", precio: 15000},
+    {id: 7, prenda: "gorra deportiva", precio: 3500},
+    {id: 8, prenda: "short de jean", precio: 4000},
+    {id: 9, prenda: "chaqueta de cuero", precio: 45000},
+    {id: 10, prenda: "vestido de seda", precio: 30000},
+];
+
+let contenido = Ropamas.querySelector("#contenido");
+
+    productos.forEach(producto => {
+        let div = Ropamas.createElement("div");
+        div.innerHTML = `
+        <span> PRENDA N°: ${producto.id} </span>
+        <h3> ARTICULO: ${producto.prenda} </h3>
+        <p> PRECIO: $ ${producto.precio} </p>
+        <button class="Agregar" data-id="${producto.id}">"Agregar al carrito"</button>
+        `;
+
+        contenido.appendChild(div);
+    })
+
+    let btns = document.querySelectorAll(".Agregar");
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let productoEncontrado = productos.find((producto) => producto.id == e.target.dataset.id);
+    carrito.push(productoEncontrado);
+    console.log(carrito);
+  });
+});
+
+
+
+
 const usuarios = [
     {nombre: "Amado",edad: 33},
     {nombre: "Juan",edad: 25},
@@ -19,6 +71,11 @@ const usuarios = [
     {nombre: "Maria",edad: 22},
     {nombre: "Jose",edad: 30}
 ]
+
+localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+let usuariosRecuperados = JSON.parse(localStorage.getItem('usuarios'));
+console.log(usuariosRecuperados);
 
 for(let i = 0; i < usuarios.length; i++){
     if(usuarios[i].edad >= 30) {
@@ -63,5 +120,43 @@ console.log(precios);
 for(let producto of precios) {
     console.log(producto);
 }
+
+const $submit = Ropamas.getElementById("submit");
+        $mensaje = Ropamas.getElementById("mensaje");
+        $mail = Ropamas.getElementById("mail");
+
+function validateInputs(){
+    const mailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mensajeRegex = /^.{1,250}$/;
+    if(!mail.value === "") {
+        alert("El mail no puede estar vacio");
+        return false;
+    } else if(!mailRegex.test($mail.value)){
+        alert("El mail no es valido");
+        return false;
+    }
+    if(!mensajeRegex.test($mensaje.value)){
+        alert("El mensaje no es valido");
+        return false;
+    }
+    if(mailRegex.test($mail.value)){
+        alert("Ingrese el formato correcto del mail");
+        $mail.style.border = "1px solid red";
+        return false;
+    }
+    if(mensajeRegex.test($mensaje.value)){
+        alert("Ingrese el formato correcto del mensaje");
+        $mensaje.style.border = "1px solid red";
+        return false;
+    }
+}
+
+//Evento
+Ropamas.addEventListener("click", (e) => {
+    if (e.target === $mensaje) {
+        e.preventDefault();
+        validateInputs();
+    }
+});
 
 alert("Gracias por comprar en nuestra tienda de ropa");
